5abfc0d98436eb84bd12133db52972659cd11bc1
alexvest <78535319+alexvest@users.noreply.github.com>
2024-03-14 12:39:31 +0100

    Uppdaterade namn

---
+++ F4 - Workshop on-lyssnare/workshop.js
@@ -0,0 +1,68 @@
+'use strict';
+let timerData = {
+    timerId : 0
+};
+
+//I workshopen/räknestugan/föreläsning 4 kommer jag att använda mig av on-attributen och då i JavaScript med anonyma funktioner.
+
+
+
+
+//Här börjar dagens kod.
+
+
+
+//Här slutar dagens kod.
+
+
+
+
+//Att öva på egen hand är att göra om lösningen till "vanliga" funktioner och lägga till on-attribut i HTML.
+
+function setFocusOnForm() {
+    /*
+        -- 1 --
+        1. Debugutskrift till Console
+        2. Sätt fokus på första textrutan i formuläret
+    */
+}
+
+function countNumberOfChars(textAreaRef) {
+    /*
+        -- 2 --
+        1. Debugutskrift till Console
+        2. Visa siffran för antalet tecken i textarea:n.
+    */
+}
+
+function resetForm() {
+    /*
+        -- 3 -- 
+        1. Debugutskrift till Console
+        2. Återställ formuläret till samma tillstånd som vid uppstart.
+    */
+}
+function checkForm() {
+    /*
+        -- 4 --
+        1. Debugutskrift till Console
+        2. Validera indata i formuläret och använda undantagshantering.
+    */
+}
+
+function startTimer() {
+    /*
+        -- 5 --
+        1. Debugutskrift till Console
+        2. Kontrollera om en timer är gång och om så är fallet avsluta timer.
+        3. Starta en ny timer som exekverar en funktion en gång i sekunden.
+    */
+}
+
+function stoppTimer() {
+    /*
+        -- 6 --
+        1. Debugutskrift till Console
+        2. Kontrollera om en timer är gång och om så är fallet avsluta timer.
+    */
+}
