f58c06de133acf0c551261522e5aa89fdab379d1
Pierre Sjöberg <pierre.sjoberg@kau.se>
2024-03-21 14:00:01 +0100

    F7 mellankod

--- F7 - Skapa element med JS/Exempel 1/slutkod/script.js
+++ F7 - Skapa element med JS/Exempel 1/slutkod/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+window.addEventListener('load', prepareColorPicker);
+
+function prepareColorPicker(){
+    
+    let fields = document.querySelectorAll('.field');
+
+    for(let i=0;i<fields.length;i++) {
+        let input = fields[i].querySelector('input');
+        let span = fields[i].querySelector('.value');
+
+        let slump = Math.floor(Math.random()*256);
+        span.textContent = slump;
+        input.value = slump;
+    }
+
+    updateBackground();
+
+}
+
+
+function updateBackground() {
+
+    let r = document.querySelector('#red').value;
+    let g = document.querySelector('#green').value;
+    let b = document.querySelector('#blue').value;
+
+    console.log('r:' + r + ' g:' +g + ' b:' +b);
+
+    let body = document.querySelector('body');
+
+    console.log("rgb(" + r + "," + g + "," + b + ")");
+
+    document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+
+
+
+
+}
