06db12f9b8575a616300648315c76849732465ac
Pierre Sjöberg <pierre.sjoberg@kau.se>
2024-03-21 14:58:28 +0100

    F7 slutkod

--- F7 - Skapa element med JS/Exempel 1/slutkod/script.js
+++ F7 - Skapa element med JS/Exempel 1/slutkod/script.js
@@ -13,6 +13,8 @@ function prepareColorPicker(){
         let slump = Math.floor(Math.random()*256);
         span.textContent = slump;
         input.value = slump;
+
+        fields[i].addEventListener('input', handleValueChange);
     }
 
     updateBackground();
@@ -30,11 +32,28 @@ function updateBackground() {
 
     let body = document.querySelector('body');
 
-    console.log("rgb(" + r + "," + g + "," + b + ")");
-
     document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 
+    let nyDiv = document.createElement('div');
+    nyDiv.setAttribute('style','border: 1px solid black');
+    nyDiv.textContent = 'Färgen är nu: ' + r +', ' + g + ', ' + b;
 
+    body.appendChild(nyDiv);
+
+
+}
+
+function handleValueChange(event) {
+
+    let field = event.currentTarget;
+
+    let inputElement = field.querySelector('input');
+    //console.log(inputElement.value);
+    let span = field.querySelector('span');
+
+    span.textContent = inputElement.value;
+
+    updateBackground();
 
 
 }
